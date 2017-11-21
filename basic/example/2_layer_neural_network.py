e1ddf51b2ef896ec6d73c290fa4b1cb47dd43e02
leolle <victor.wuv@gmail.com>
2017-11-21 19:36:11 +0800

    layer name

--- basic/example/2_layer_neural_network.py
+++ basic/example/2_layer_neural_network.py
@@ -33,26 +33,26 @@ def two_layer_nn():
     for iter in range(10000):
 
         # forward propagation
-        l0 = X
+        layer_0 = X
         # predict
-        l1 = nonlin(np.dot(l0, syn0))
+        layer_1 = nonlin(np.dot(layer_0, syn0))
 
         # error, how much did we miss?
-        l1_error = y - l1
+        layer_1_error = y - layer_1
 
         # multiply how much we missed by the
-        # slope of the sigmoid at the values in l1.
+        # slope of the sigmoid at the values in layer_1.
         # reducing the error of high confidence predictions.
-        # the more confident the prediction, the smaller the l1 error and the
-        # derivative of l1.
-        l1_delta = l1_error * nonlin(l1, True)
+        # the more confident the prediction, the smaller the layer_1 error and the
+        # derivative of layer_1.
+        layer_1_delta = layer_1_error * nonlin(layer_1, True)
 
         # update weights
-        print(l1_delta[0], syn0[0])
-        syn0 += np.dot(l0.T, l1_delta)
+        # print(layer_1_delta[0], syn0[0])
+        syn0 += np.dot(layer_0.T, layer_1_delta)
 
     print("Output After Training:")
-    print(l1)
+    print(layer_1)
 
     # test input dataset
     X_test = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
@@ -75,9 +75,9 @@ def three_layer_nn():
     for j in range(60000):
 
         # forward propagation
-        l0 = X
-        l1 = nonlin(np.dot(l0, syn0))
-        l2 = nonlin(np.dot(l1, syn1))
+        layer_0 = X
+        layer_1 = nonlin(np.dot(layer_0, syn0))
+        l2 = nonlin(np.dot(layer_1, syn1))
 
         # how much did we miss the target value?
         l2_error = y - l2
@@ -89,15 +89,15 @@ def three_layer_nn():
         # were we really sure? if so, don't change too much.
         l2_delta = l2_error * nonlin(l2, deriv=True)
 
-        # how much did each l1 value contribute to the l2 error (according to the weights)?
-        l1_error = l2_delta.dot(syn1.T)
+        # how much did each layer_1 value contribute to the l2 error (according to the weights)?
+        layer_1_error = l2_delta.dot(syn1.T)
 
-        # in what direction is the target l1?
+        # in what direction is the target layer_1?
         # were we really sure? if so, don't change too much.
-        l1_delta = l1_error * nonlin(l1, deriv=True)
+        layer_1_delta = layer_1_error * nonlin(layer_1, deriv=True)
         # w(new) = w(old) − η · (y − t) · y(1 − y) · x
-        syn1 += l1.T.dot(l2_delta)
-        syn0 += l0.T.dot(l1_delta)
+        syn1 += layer_1.T.dot(l2_delta)
+        syn0 += layer_0.T.dot(layer_1_delta)
 
     print("Output After Training:")
     print(l2)
