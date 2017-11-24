70421ce6db661607053b9d21a11a250186b965df
leolle <victor.wuv@gmail.com>
2017-11-24 13:51:04 +0800

    update notes

--- word_embedding/GloVe/GloVe-1.2/load_glove_vectors.py
+++ word_embedding/GloVe/GloVe-1.2/load_glove_vectors.py
@@ -21,14 +21,16 @@ def loadGloveModel(gloveFile):
 
 
 path = '/home/weiwu/projects/deep_learning/word_embedding/GloVe/GloVe-1.2'
+glove_path = '/home/weiwu/share'
 name = 'vectors.bin'
 file_name = os.path.join(path, name)
-# model = word2vec.Word2Vec.load_word2vec_format(
-#     file_name, binary=True, unicode_errors='ignore')
-# model = KeyedVectors.load_word2vec_format(file_name, binary=True)
-txt_name = '/home/weiwu/share/glove.6B.100d.txt'
-model = loadGloveModel(txt_name)
-
+txt_name = 'vectors.txt'
+w2v_txt_name = 'vectors.w2vformat.txt'
+txt_file = os.path.join(path, txt_name)
+w2v_txt_file = os.path.join(path, w2v_txt_name)
+# model = loadGloveModel(txt_name)
 words = pd.read_table(
-    txt_name, sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
-words_matrix = words.as_matrix()
+    w2v_txt_file, sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
+# words_matrix = words.as_matrix()
+model = KeyedVectors.load_word2vec_format(
+    os.path.join(path, 'vectors.w2vformat.txt'), binary=True)
