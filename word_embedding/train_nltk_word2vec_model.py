cbac7f5a6406191de3c4605ed55fa4476b3810e5
leolle <victor.wuv@gmail.com>
2017-11-29 19:12:45 +0800

    parse DJA news

--- word_embedding/train_nltk_word2vec_model.py
+++ word_embedding/train_nltk_word2vec_model.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint  # pretty-printer
+from collections import defaultdict
 import numpy as np
 from gensim.models import word2vec
 from gensim.models import KeyedVectors
@@ -20,14 +22,28 @@ sentences = brown.words()
 # print(model.most_similar(['brown']))
 reddit_news = pd.read_csv('/home/weiwu/share/deep_learning/data/RedditNews.csv')
 DJA_news = pd.read_csv(
-    '/home/weiwu/share/deep_learning/data/Combined_News_DJIA.csv')
+    '/home/weiwu/share/deep_learning/data/Combined_News_DJIA_parsed.csv')
 na_str_DJA_news = DJA_news.iloc[:, 2:].values
 na_str_DJA_news = na_str_DJA_news.flatten()
+ls_str_DJA_news = na_str_DJA_news.tolist()
+# na_str_DJA_news = na_str_DJA_news.reshape(-1,1)
 na_str_reddit_news = reddit_news.News.values
-sentences_reddit = [s.encode('utf-8').split() for s in na_str_reddit_news]
-sentences_DJA = [s.encode('utf-8').split() for s in na_str_DJA_news]
-# for s in na_str_DJA_news:
-#     if s is float:
-#         continue
-#     [s.encode('utf-8').split()]
-model = word2vec.Word2Vec(sentences_DJA, size=200)
+
+# sentences_reddit = [s.encode('utf-8').split() for s in na_str_reddit_news]
+# sentences_DJA = [s.encode('utf-8').split() for s in na_str_DJA_news]
+
+# remove common words and tokenize
+stoplist = set('for a of the and to in'.split())
+texts = [[
+    word for word in str(document).decode('utf-8').lower().split()
+    if word not in stoplist
+] for document in ls_str_DJA_news]
+
+# remove words that appear only once
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+texts = [[token for token in text if frequency[token] > 1] for text in texts]
+# pprint(texts)
+model = word2vec.Word2Vec(texts, size=200)
