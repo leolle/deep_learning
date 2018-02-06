af7607510b7780bc0fb9a3a100d73c7e7f517c42
leolle <victor.wuv@gmail.com>
2018-02-06 17:28:09 +0800

    add skill 2 graph function

--- knowledge_graph/database/test_skill_2_graph.py
+++ knowledge_graph/database/test_skill_2_graph.py
@@ -3,12 +3,6 @@
 """
 """
 import logging
-import os
-import re
-import sys
-from time import time, sleep
-from timeit import default_timer
-from tqdm import tqdm
 from ylib import ylog
 from lib.gftTools import gftIO
 from lib.gftTools.proto import graphUpload_pb2
@@ -22,9 +16,8 @@ ylog.info("start")
 
 def skill_result_2_graph(resp_run_node_action):
     """
-    convert skill_pb2.RespRunNodeAction to graphUpload.proto.GraphUploadRequest.Graph
+    convert skill_pb2.RespRunNodeAction to graphUpload.proto.Graph
     so that in python we have only one graph format.
-
 
     Keyword Arguments:
     resp_run_node_action -- skill_pb2.RespRunNodeAction, result from get_graph_from_neo4j
@@ -33,10 +26,6 @@ def skill_result_2_graph(resp_run_node_action):
     graphUpload_pb2.graph
     """
     graph_upload_request = graphUpload_pb2.GraphUploadRequest()
-    graph_upload_request.nodeAction4Duplication = graphUpload_pb2.Action4Duplication.Value(
-        'UPDATE')
-    graph_upload_request.edgeAction4Duplication = graphUpload_pb2.Action4Duplication.Value(
-        'UPDATE')
 
     for n in resp_run_node_action.graphs[0].graph.nodes:
         node = graph_upload_request.graph.nodes.add()
@@ -59,31 +48,30 @@ test_pwd = 'gft'
 gs_call = gftIO.GSCall(test_url, test_user_name, test_pwd)
 
 graph = gs_call.get_graph_from_neo4j('D81A370D094AAAC4A018AFBE409A645A')
-graph_upload_request = graphUpload_pb2.GraphUploadRequest()
-# n = graph.graphs[0].graph.nodes[0]
-# node = graph_upload_request.graph.nodes.add()
-# for p in n.node_prop.props.entries:
-#     if p.key == '_type':
-#         node.props.type = p.value
-#     elif p.key == '_s_graph_upload_tag':
-#         graph_upload_request.uploadTag = p.value
-#     else:
-#         field = node.props.props.entries.add()
-#         field.key = p.key
-#         field.value = p.value
-graph_upload_request.nodeAction4Duplication = graphUpload_pb2.Action4Duplication.Value(
-    'UPDATE')
-graph_upload_request.edgeAction4Duplication = graphUpload_pb2.Action4Duplication.Value(
-    'UPDATE')
-
+graph_message = graphUpload_pb2.Graph()
+# edges = graph.graphs[0].graph
+nodes = {}
 for n in graph.graphs[0].graph.nodes:
-    node = graph_upload_request.graph.nodes.add()
+    node = graph_message.nodes.add()
+    print(n.nid)
     for p in n.node_prop.props.entries:
         if p.key == '_type':
             node.props.type = p.value
-        elif p.key == '_s_graph_upload_tag':
-            graph_upload_request.uploadTag = p.value
+        elif p.key == '_gid':
+            nodes[n.nid] = p.value
         else:
             field = node.props.props.entries.add()
             field.key = p.key
             field.value = p.value
+edges = {}
+for e in graph.graphs[0].graph.edges:
+    edge = graph_message.edges.add()
+    edge.props.type = e.type
+    edges[e.eid] = {'sn_id': e.sn_id, 'en_id': e.en_id}
+    for p in e.edge_prop.props.entries:
+        if p.key == '_type':
+            edge.props.type = p.value
+    edge.startNodeID.gid = nodes[e.sn_id]
+    edge.endNodeID.gid = nodes[e.en_id]
+
+graph_from_skill = gs_call.skill_result_2_graph(graph)
