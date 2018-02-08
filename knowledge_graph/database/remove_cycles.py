c9ab2c66a2fa00363e378cb6d62c0157026e8b86
leolle <victor.wuv@gmail.com>
2018-02-08 17:36:11 +0800

    update notes

--- knowledge_graph/database/remove_cycles.py
+++ knowledge_graph/database/remove_cycles.py
@@ -17,34 +17,41 @@ import networkx as nx
 
 ylog.set_level(logging.DEBUG)
 ylog.console_on()
-
+ylog.filelog_on("cycles")
 graph = nx.read_gexf('whole_edges.rm_de.gexf')
 ls_nodes = list(graph.nodes)
 counter = 0
 total_nodes_num = 287966
 rm_counter = 0
-while True:
-    removed_counter = 0
-    print('rm cycles loops number %s' % rm_counter)
+try:
+    while True:
+        removed_counter = 0
+        ylog.debug('rm cycles loops number %s' % rm_counter)
 
-    for node in tqdm(ls_nodes):
-        try:
-            # remove direct edge:
-            ls_loop = nx.find_cycle(graph, node)
-            # print(ls_loop)
-            if len(ls_loop) == 2:
-                if ls_loop[0][0] == ls_loop[1][1] and ls_loop[0][1] == ls_loop[1][0]:
-                    graph.remove_edge(ls_loop[0][0], ls_loop[0][1])
-            elif len(ls_loop) > 2:
-                graph.remove_edge(ls_loop[-1][0], ls_loop[-1][1])
-            # dict_loop[node].append(nx.find_cycle(G, node)[0])
-            counter = 0
-            removed_counter += 1
-        except nx.NetworkXNoCycle:
-            counter += 1
-            pass
-    if counter == (total_nodes_num - 1):
-        break
-    print('rm cycles number %s' % removed_counter)
-    rm_counter += 1
+        for node in tqdm(ls_nodes):
+            try:
+                # remove direct edge:
+                ls_loop = nx.find_cycle(graph, node)
+                ylog.debug(ls_loop)
+                if len(ls_loop) == 2:
+                    if ls_loop[0][0] == ls_loop[1][1] and ls_loop[0][1] == ls_loop[1][0]:
+                        graph.remove_edge(ls_loop[0][0], ls_loop[0][1])
+                elif len(ls_loop) > 2:
+                    graph.remove_edge(ls_loop[-1][0], ls_loop[-1][1])
+                # dict_loop[node].append(nx.find_cycle(G, node)[0])
+                counter = 0
+                removed_counter += 1
+            except nx.NetworkXNoCycle:
+                counter += 1
+                pass
+        if removed_counter < 5:
+            break
+        ylog.debug('rm cycles number %s' % removed_counter)
+        rm_counter += 1
+except KeyboardInterrupt:
+    nx.write_gexf(graph, 'whole_edges.no_loops.gexf')
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
 nx.write_gexf(graph, 'whole_edges.no_loops.gexf')
