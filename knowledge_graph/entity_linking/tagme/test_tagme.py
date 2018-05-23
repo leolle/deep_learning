85eeacd5c2c27e86f80265389b68d76cc181697e
leolle <victor.wuv@gmail.com>
2018-05-23 14:52:11 +0800

    add entity linking

---
+++ knowledge_graph/entity_linking/tagme/test_tagme.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import tagme
+import wikipedia
+# Set the authorization token for subsequent calls.
+tagme.GCUBE_TOKEN = "1e1a9d3f-47ab-4df1-a063-45301072978f-843339462"
+
+TEXT = 'The man saw a Jaguar speed on the highway'
+TEXT = 'The prey saw the jaguar cross the jungle.'
+TEXT = 'Consumer protection legislation typically labels vehicles as "lemons" if the same problem recurs despite multiple repair attempts.'
+TEXT = 'Democrats not invited to DOJ briefing on FBI informant'
+text_annotations = tagme.annotate(TEXT)
+
+# Print annotations with a score higher than 0.1
+for ann in text_annotations.get_annotations(0.1):
+    print(ann)
+    print(wikipedia.summary(ann.entity_title)[:250] + '...')
+# tomatoes_mentions = tagme.mentions(
+#     "I definitely like ice cream better than tomatoes.")
+
+# for mention in tomatoes_mentions.mentions:
+#     print(mention)
