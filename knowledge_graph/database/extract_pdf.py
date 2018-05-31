de1e8752926e1727b5fb6eafa6f37f2a91e7ddd7
leolle <victor.wuv@gmail.com>
2018-05-31 17:21:38 +0800

    to test crossref accuracy

--- knowledge_graph/database/extract_pdf.py
+++ knowledge_graph/database/extract_pdf.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # from PyPDF2 import PdfFileReader
+from scihub2pdf import download as dl
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from crossref.restful import Works
@@ -13,50 +14,8 @@ ylog.set_level(logging.DEBUG)
 ylog.console_on()
 ylog.filelog_on("app")
 
-# def get_info(path):
-#     with open(path, 'rb') as f:
-#         pdf = PdfFileReader(f)
-#         info = pdf.getDocumentInfo()
-#         number_of_pages = pdf.getNumPages()
-
-#     print(info)
-
-#     author = info.author
-#     creator = info.creator
-#     producer = info.producer
-#     subject = info.subject
-#     title = info.title
-
-
-def text_extractor(path):
-    with open(path, 'rb') as f:
-        pdf = PdfFileReader(f)
-
-        # get the first page
-        page = pdf.getPage(1)
-        print(page)
-        print('Page type: {}'.format(str(type(page))))
-
-        text = page.extractText()
-        print(text)
-
-
-get_info(path)
-
-path = '/home/weiwu/share/deep_learning/docs/word_embedding/Distributed Representations of Words and Phrases.pdf'
-path = '/home/weiwu/share/deep_learning/docs/knowledge_graph/15. Entity Linking for Biomedical Literature.pdf'
-with open(path, 'rb') as f:
-    # pdf = PdfFileReader(f)
-    # info = pdf.getDocumentInfo()
-    parser = PDFParser(f)
-    doc = PDFDocument(parser)
-    print(doc.info)
-
 works = Works()
-# print(works.doi('10.1021/ma035065b'))
 
-# w1 = works.query(
-#     title='zika', author='johannes', publisher_name='Wiley-Blackwell')
 title = """Neural Architectures for Named Entity Recognition"""
 w1 = works.query(title).sort('relevance').order('desc')
 i = 0
@@ -70,8 +29,8 @@ for item in w1:
     if SequenceMatcher(a=title, b=t).quick_ratio() > 0.9:
         items = item
         # items.append(item)
-from scihub2pdf import download as dl
 dl.download_from_doi(items['DOI'])
+
 # from crossref.restful import Works, Etiquette
 
 # my_etiquette = Etiquette('My Project Name', 'My Project version',
@@ -89,3 +48,46 @@ dl.download_from_doi(items['DOI'])
 
 # for i in works.sample(5).select('DOI'):
 #     print(i)
+import os
+from urllib.request import Request, urlopen, HTTPError, URLError
+
+
+def download_url(url):
+    try:
+        fn = url.split("/")[-1]
+        fn_download = os.path.join(output_directory, fn)
+        with open(fn_download, "wb") as f:
+            request = Request(sanitize_url(url))
+            request.add_header("User-Agent", "Mozilla/5.0 (compatible; "
+                               "MSIE 9.0; Windows NT 6.1; Trident/5.0)")
+            response = urlopen(request, context=ssl_unverified_context)
+            status_code = response.getcode()
+            if status_code == 200:
+                f.write(urlopen(request).read())
+                colorprint(OKGREEN, "Downloaded '%s' to '%s'" % (url,
+                                                                 fn_download))
+            else:
+                colorprint(FAIL, "Error downloading '%s' (%s)" % (url,
+                                                                  status_code))
+    except HTTPError as e:
+        colorprint(FAIL, "Error downloading '%s' (%s)" % (url, e.code))
+    except URLError as e:
+        colorprint(FAIL, "Error downloading '%s' (%s)" % (url, e.reason))
+    except Exception as e:
+        colorprint(FAIL, "Error downloading '%s' (%s)" % (url, str(e)))
+
+
+# Create directory
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+    vprint("Created directory '%s'" % output_directory)
+
+try:
+    pool = ThreadPool(5)
+    pool.map(download_url, urls)
+    pool.wait_completion()
+
+except Exception as e:
+    print(e)
+except KeyboardInterrupt:
+    pass
