ba2356b68face57727d1c891a9b5c8743861ae4a
leolle <victor.wuv@gmail.com>
2018-08-10 19:02:53 +0800

    update notes

--- knowledge_graph/entity_linking/tagme/test_tagme.py
+++ knowledge_graph/entity_linking/tagme/test_tagme.py
@@ -5,14 +5,14 @@ import wikipedia
 tagme.GCUBE_TOKEN = "1e1a9d3f-47ab-4df1-a063-45301072978f-843339462"
 
 TEXT = 'The man saw a Jaguar speed on the highway'
-TEXT = 'The prey saw the jaguar cross the jungle.'
 TEXT = 'Consumer protection legislation typically labels vehicles as "lemons" if the same problem recurs despite multiple repair attempts.'
-TEXT = 'Democrats not invited to DOJ briefing on FBI informant'
-TEXT = 'Zuckerberg avoided tough questions thanks to short EU testimony format'
+# TEXT = 'Democrats not invited to DOJ briefing on FBI informant'
+# TEXT = 'Zuckerberg avoided tough questions thanks to short EU testimony format'
+TEXT = 'The prey saw the jaguar cross the jungle.'
 text_annotations = tagme.annotate(TEXT)
 
 # Print annotations with a score higher than 0.1
-for ann in text_annotations.get_annotations(0.1):
+for ann in text_annotations.get_annotations(0.001):
     print(ann)
     print(wikipedia.summary(ann.entity_title)[:250] + '...')
 
